871152f488dbc8df0f44870c84656864c90b5b49
ritviksaxena <78605818+ritviksaxena4@users.noreply.github.com>
2025-05-23 20:35:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "aws_s3_bucket" "example1" {
   bucket = "my-tf-test-bkt-23may2025206"
 }
 
+
+
+resource "aws_instance" "my-second-server" {
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+}
